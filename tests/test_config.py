781a184bf8b576271dbea3897a4af63dc5e077af
Oskar Skoog <oskar.skoog@agama.tv>
2025-07-19 00:05:48 +0300

    Support python 3.9

--- tests/test_config.py
+++ tests/test_config.py
@@ -6,7 +6,6 @@ import sys
 sys.path.insert(0, str(Path(__file__).parent.parent))
 from ctenv import (
     _load_config_file,
-    ConfigFile,
     substitute_template_variables,
     substitute_in_context,
 )
